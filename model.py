27c9beb237285f53a0f7ca7833d928dc708a28a0
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-01-11 21:28:18 -0500

    added imports

---
+++ model.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix, classification_report
