6ec2cbab642d97a241799fc2abbb06b55d4a6150
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-01-11 22:25:52 -0500

    finished model

--- model.py
+++ model.py
@@ -4,3 +4,31 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix, classification_report
+
+df = pd.read_csv("500hits.csv", encoding="latin-1")
+
+X = df.drop(columns=["PLAYER", "HOF"])
+y = df["HOF"]
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=11)
+
+model = LogisticRegression(C=0.3, solver='liblinear', max_iter=1000)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+
+print("Confusion Matrix: ")
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+print("\nClassification Report: ")
+cr = classification_report(y_test, y_pred)
+print(cr)
+
+print(f"Model Accuracy: {accuracy:.2f}")
